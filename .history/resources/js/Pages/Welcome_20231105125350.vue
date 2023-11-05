3f6b923c59f6301aee76e4eefe9902a4f8ee30d8
unknown <meroo1995saggaf@outlook.com>
2023-11-05 13:07:55 +0300

    start the project

---
+++ .history/resources/js/Pages/Welcome_20231105125350.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import XLayout from '@/Layouts/XLayout.vue';
+
+</script>
+
+<template>
+    <Head title="Welcome" />
+    
+     <XLayout>
+        <div class="text-red-900">Welcome</div>
+     </XLayout>
+
+<template/>    
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import XLayout from '@/Layouts/XLayout.vue';
+
+</script>
+
+<template>
+    <Head title="Welcome" />
+    
+     <XLayout>
+        <div class="bg-red-900">Welcome</div>
+     </XLayout>
+
+    </template>    
